5863c5f24ffee939a180dc92242730825c958243
myroman-wuLtCL <myroman@protonmail.com>
2023-04-24 14:47:12 -0400

    updated functions index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cTable = require("console.table");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  // TODO: Add MySQL password here
   password: "rootROOT",
   database: "employee_tracker_db"
 });
@@ -13,8 +12,8 @@ function start() {
     .prompt([
   {
     type: "list",
-    message: "How can I help you?"
-        choices: [
+    message: "How can I help you?",
+    choices: [
       "Add employee",
       "Add department",
       "Add position",
@@ -55,5 +54,49 @@ function start() {
 })
 
 };
+function viewAllEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+  function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+})
+}
+function addEmployee() {
+  db.query(
+    "select * from employee", (err, empRes) => {
+      const employees = empRes.map(employee) => {
+        return employee.first_name + employee.last_name;
+      });
+      db.query(
+        "select * from position", (err, emPos) => {
+          const positions = positionRes.map(position) => {
+            return position.title;
+          });
+          inquirer
+          prompt([{
+            type: "input",
+            message: "Please enter first name of employee to be added.",
+            name: "first_name"
+          },
+          {
+            type: "input",
+            message: "Please enter last name of employee to be added.",
+            name: "last_name"
+          },
+          {
+            type: "list",
+            message: "Please choose the new employees position.",
+            name: "position_id",
+            choices: positions
+          }
+        ]).then(res) => {
+          const { first_name, last_name } = res;
+          const 
+
+          }
+
+
+          }
 
 
